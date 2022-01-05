68035d6f6cb4ca84c91b68ce28451fe78fba047c
Thuong Pham <timphamvn33@gmail.com>
2022-01-05 12:27:51 -0500

    Procfile

--- app.js
+++ app.js
@@ -62,9 +62,13 @@ app.post("/", function(req, res){
 
   });
 
-  
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function(){
+
+app.listen(port, function(){
     console.log("server runs")
 })
 
